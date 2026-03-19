cb1c751a3dcf62dc2def5ca4e1964ebb7e7cc4c8
Brian Battauz <brian.nbattauz@gmail.com>
2026-03-19 17:58:49 -0300

    fix: correccion de rol

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.include_router(api_router, prefix="/api/v1")
 @app.get("/")
 def read_root():
     return {
-        "sistema": "Arlestin API",
+        "sistema": "Drip Gestión API v1.4.1",
         "estado": "Operativo",
         "multitenant": True
     }
